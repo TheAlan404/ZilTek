c4e678408185a699520cbefcd32f7d642c117458
Dennis <43997085+TheAlan404@users.noreply.github.com>
2024-01-09 11:23:55 +0300

    Update meta.tsx

--- src/meta.tsx
+++ src/meta.tsx
@@ -1,5 +1,5 @@
 export const VERSION = "v2";
-export const BUILD = "060124";
+export const BUILD = "080124";
 export const DEFAULT_RELAY = "ws://ziltek.kuylar.dev";
 export const UPDATE_URL = "https://raw.githack.com/TheAlan404/ZilTekProject/main/metadata.json";
 export const WEBSITE = "https://thealan404.github.io/ziltekproject";
